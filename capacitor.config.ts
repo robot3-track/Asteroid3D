03418d2445571a3378310a855dd0fb15b523146e
robot3-track <yohanyinyuchang@gmail.com>
2026-07-22 16:03:07 -0700

    update capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.asteroid.simulator',
+  appName: 'Asteroid Simulator',
+  webDir: 'out',              
+  bundledWebRuntime: false
+};
+
+export default config;

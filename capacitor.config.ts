c00cb44eb8b48baecd37e8de7d6e4e9348f7fea2
robot3-track <yohanyinyuchang@gmail.com>
2026-07-22 20:12:25 -0700

    Fix webapp: Remove deprecated bundledWebRuntime property from capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.asteroid.simulator',
   appName: 'Asteroid Simulator',
-  webDir: 'out',              
-  bundledWebRuntime: false
+  webDir: 'out'
 };
 
 export default config;
